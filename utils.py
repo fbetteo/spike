baf87d1cd012623bc81565e6eee91c188e018373
Franco <francobetteo@gmail.com>
2021-07-29 01:21:09 -0300

    avance

--- utils.py
+++ utils.py
@@ -16,10 +16,10 @@ def plot_hist_precipitaciones(df, region=None, ax=None):
 def plot_line_precipitaciones(df, region=None, desde=None, hasta=None, ax=None):
     """ Validamos por separado la combinación destino/desde y destino/hasta porque
     puede darse que exista un extremo pero no el otro"""
-    valid_query_from = ((df.region.str.contains(region)) &
-                        (df.date.str.contains(desde))).sum()
-    valid_query_to = ((df.region.str.contains(region)) &
-                      (df.date.str.contains(hasta))).sum()
+    valid_query_from = any((df.region == region) &
+                           (df.date == desde))
+    valid_query_to = any((df.region == region) &
+                         (df.date == hasta))
 
     if not ((valid_query_from + valid_query_to) == 2):
         print("La región o fechas no son válidas")
@@ -31,3 +31,36 @@ def plot_line_precipitaciones(df, region=None, desde=None, hasta=None, ax=None):
         max_tick = max(ax.get_xticks())
         ax.set_xticks(
             range(0, max_tick + 1, round(max_tick * 0.05)))
+
+
+def plot_precipitaciones_mensuales(df, region=None, years=None, ax=None):
+    """ Validamos por separado la combinación destino/desde y destino/hasta porque
+    puede darse que exista un extremo pero no el otro"""
+    year0 = years[0]
+    year1 = years[1]
+    df['year'] = [int(x[0]) for x in df['date'].str.split("-")]
+    df['month'] = [int(x[1]) for x in df['date'].str.split("-")]
+
+    valid_query_from = any((df.region == region) &
+                           (df.year == year0))
+    valid_query_to = any((df.region == region) &
+                         (df.year == year1))
+
+    if not ((valid_query_from + valid_query_to) == 2):
+        print("La región o fechas no son válidas")
+    else:
+        df = df.query(
+            'region==@region and year in @years').sort_values('date')
+        sns.lineplot(data=df, x='month',
+                     y='precipitaciones', hue='year', palette="Reds", ax=ax)
+        ax.set_title(region)
+        ax.set_xticklabels(['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                            'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'])
+
+
+def plot_pbi(df, series, desde, hasta, ax=None):
+    df = df.query(
+        'variable in @series and Periodo >= @desde and Periodo <= @hasta').sort_values('Periodo')
+
+    sns.lineplot(data=df, x="Periodo", y="valor", hue="variable", ax=ax)
+    ax.set_title(series)
